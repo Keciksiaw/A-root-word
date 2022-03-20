ed81cf8c689add32bc13e42da4b398fb888413ec
frogindreams <frogharvard@gmail.com>
2022-03-21 00:05:02 +0700

    I've fixed two latest bugs

--- src/modules/searcherOfRootWords.h
+++ src/modules/searcherOfRootWords.h
@@ -10,6 +10,8 @@ void search_rw_longest(std::vector<std::string> root_words) {
     std::string longest_word;
     int count_lw_main = 0;
     int count_lw_sub = 0;
+    int length_of_lw_main = 0;
+    int length_of_lw_sub = 0;
 
     for (auto current_word : root_words) {
         const auto begin_lw = 0;
@@ -17,6 +19,7 @@ void search_rw_longest(std::vector<std::string> root_words) {
 
         for (auto end_lw = (current_word.size() - 1); end_lw > 0; end_lw--) {
             sub_lw = current_word.substr(begin_lw, end_lw);
+            length_of_lw_sub = sub_lw.size();
 
             for (auto inner_cw : root_words) {
                 if ( inner_cw.find(sub_lw) != std::string::npos && inner_cw[0] == sub_lw[0]) {
@@ -24,12 +27,14 @@ void search_rw_longest(std::vector<std::string> root_words) {
                 }
             }
 
-            if ( count_lw_sub > count_lw_main ) {
+            if ( (count_lw_sub > 1) && (length_of_lw_sub > length_of_lw_main) ) {
                 count_lw_main = count_lw_sub;
                 longest_word = sub_lw;
+                length_of_lw_main = length_of_lw_sub;
             } 
 
             count_lw_sub = 0;
+            length_of_lw_sub = 0;
         }
     }
 
@@ -57,9 +62,12 @@ void search_rw_major(std::vector<std::string> root_words) {
     for (auto current_word : root_words) {
         const auto begin_mw = 0;
         std::string sub_mw;
+        int length_of_mw_main = 0;
+        int length_of_mw_sub = 0;
 
         for (auto end_mw = 1; end_mw < current_word.size(); end_mw++) {
             sub_mw = current_word.substr(begin_mw, end_mw);
+            length_of_mw_sub = sub_mw.size();
 
             for (auto inner_cw : root_words) {
                 if ( inner_cw.find(sub_mw) != std::string::npos && inner_cw[0] == sub_mw[0]) {
@@ -67,12 +75,14 @@ void search_rw_major(std::vector<std::string> root_words) {
                 }
             }
 
-            if ( count_mw_sub > count_mw_main ) {
+            if ( (count_mw_sub > count_mw_main) && (length_of_mw_sub > length_of_mw_main) ) {
                 count_mw_main = count_mw_sub;
                 major_word = sub_mw;
+                length_of_mw_main = length_of_mw_sub;
             } 
 
             count_mw_sub = 0;
+            length_of_mw_sub = 0;
         }
     }
 
